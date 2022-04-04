d977b5885e6b127f22ce3adcc440e248d1cf4876
angrybirds888 <ash06scott@gmail.com>
2022-04-04 17:58:24 +0500

    initial commit

---
+++ Harry Potter/MainBoardViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  Harry Potter
+//
+//  Created by ASDiMac5 on 14/03/22.
+//
+
+import UIKit
+
+class MainBoardViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundCountingLabel: UILabel!
+   
+    
+    
+// guessNumber responds for guessing right number
+    var guessNumber : Int = Int.random(in: 1...100)
+    var score : Int = 0
+    var round : Int = 1
+    
+    override func viewDidLoad() {
+        let normalThumbImage = UIImage(named:"SliderThumb-Normal")
+        let highlightedThumbImage = UIImage (named: "SliderThumb-Highlighter")
+      
+        slider.setThumbImage(normalThumbImage, for : .normal)
+        slider.setThumbImage(highlightedThumbImage, for : .highlighted)
+        
+       setUp ()
+        
+    
+    }
+    
+    @IBAction func didTapSelectButton(_ sender: Any) {
+        // 1. Read the number from slider
+        let sliderValue : Float = slider.value
+        
+        // 2.Translate this number in Int = (sliderValueInt), in order to compare two whole numbers
+        let sliderValueInt : Int = Int(sliderValue)
+        
+        // 3. Compare these numbers and if they resemble then they must be output in console the info about it
+        let isValueEquals : Bool = sliderValueInt == guessNumber
+        if isValueEquals {
+            print ("The number is guessed")
+            score = score + 1
+            scoreLabel.text = String("score:\(score)")
+        } else {
+            print("The number is chosen\(sliderValueInt)")
+        }
+        round += 1
+        roundCountingLabel.text = "Round " + String(round)
+        if round == 10 {
+            setUp()
+        }
+         setUpGuessingNumber()
+    }
+    
+    @IBAction func didTapRestartButton(_ sender: Any) {
+        print("This restarButton is tapped")
+        setUp()
+    }
+    
+    func setUp() {
+        // settings of game
+        // 1. Setting the purpose of slider equals 50
+        slider.value = 50
+        
+        
+        // 2. Setting the number which will be guessed
+        setUpGuessingNumber()
+        taskLabel.textColor = .white
+        
+        // 3. Nullify the scores
+        score = 0
+        scoreLabel.text = "Score : " + String(score)
+        scoreLabel.textColor = .white
+        
+        // Nulligy the round
+        round = 0
+        roundCountingLabel.text = "Round :" + String(round)
+        
+        
+    }
+    
+    func setUpGuessingNumber() {
+        guessNumber = Int.random(in: 1...100)
+        taskLabel.text = "Try to guess number : \(guessNumber)"
+    }
+}
+
+
+
+
+
+
+
+ 
